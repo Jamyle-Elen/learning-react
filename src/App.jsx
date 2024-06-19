19247799182743ad7c977544ea5fa95b3dece4ad
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-19 20:42:44 -0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Página de Login</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, libero deleniti porro saepe beatae voluptatem? Deserunt accusamus sunt vel non commodi magni eum dicta similique quo, mollitia nihil cum voluptas.</p>
+    </div>
+  )
+}
+
+export default App

7b8f979e44af58ba4fbc5fbae915fabb614c8305
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-20 21:25:19 -0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Login from './Login/Login'
 
 function App() {
 
   return (
     <div className="App">
-      <p>Página de Login</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, libero deleniti porro saepe beatae voluptatem? Deserunt accusamus sunt vel non commodi magni eum dicta similique quo, mollitia nihil cum voluptas.</p>
+    <Login />
     </div>
   )
 }
---
+++ src/Login/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className="container">
+        <form>
+            <h1>Acesse o sistema</h1>
+            <div>
+                <input type="email" placeholder='Digite seu email...' />
+            </div>
+            <div>
+                <input type="senha" placeholder='Digite sua senha...' />
+            </div>
+            <button>Enviar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
